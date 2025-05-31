19180501ff43408a4b40bf9598118b3ab4392228
Pamela Coelho <pamelacoelho117@gmail.com>
2025-05-31 14:39:26 -0300

    feat: Versão inicial do portfólio HTML/CSS existente

---
+++ Javascript/teste.js/meu_script.js
@@ -0,0 +1,8 @@
+//prestar muita atenção nos erros de digitação dos códigos
+//se eu criar uma variável com letra minúscula tenho que usar minuscula, assim tambem com letra maiuscula
+//use nomes coerentes para as variáveis ex: salário: variável, salário
+//typeof serve para avisar o tipo de variável que está sendo usada: number, functio, object, string e assim por diante
+//ex: typeof [alguma coisa] vai dizer que é object, "" vai dizer string etc
+//aula 5<< tipos primitivos, variáveis, atribuição
+// aula 6 << tratamento de dados
+// o prompt retorna como string mesmo que eu tenha jogado um numero, aula 6 time15:24m
